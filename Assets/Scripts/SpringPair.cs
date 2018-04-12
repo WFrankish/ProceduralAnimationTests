0bf07650e1debedaa1aadd88341316f914447529
William Frankish <wfrankish@gmail.com>
2018-04-12 19:22:22 +0100

    create animal made out of springs

---
+++ Assets/Scripts/SpringPair.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SpringPair : MonoBehaviour {
+
+    private Rigidbody _self;
+
+    public Rigidbody Other;
+
+    public float Stiffness;
+
+    public float RestOffset;
+
+	// Use this for initialization
+	void Start () {
+        _self = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = Other.transform.position -_self.transform.position;
+        Vector3 difference = offset - (offset.normalized * RestOffset);
+        Vector3 force = Stiffness * difference;
+
+        _self.AddForce(force / 2f);
+        Other.AddForce(-force / 2f);
+    }
+}

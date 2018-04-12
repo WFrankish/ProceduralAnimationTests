1321da4ae5a7fe2fc44288c7049a3e0cf05939ed
William Frankish <wfrankish@gmail.com>
2018-04-12 20:05:23 +0100

    botch rotation matching, but commit anyway because its hilarious

---
+++ Assets/Scripts/SwivelPair.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SwivelPair : MonoBehaviour
+{
+
+    private Rigidbody _self;
+
+    public Rigidbody Other;
+
+    public float Stiffness;
+
+    public float RestAngleX;
+    public float RestAngleZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        _self = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = _self.transform.InverseTransformVector(Other.transform.position);
+        float offsetX = Vector3.Angle(Vector3.forward, offset);
+        float offsetZ = Vector3.Angle(Vector3.right, offset);
+
+        float diffX = RestAngleX - offsetX;
+        float diffZ = RestAngleZ - offsetZ;
+
+        _self.AddForce(_self.transform.right * diffX / 2f);
+        Other.AddForce(-_self.transform.right * diffX / 2f);
+
+        _self.AddForce(_self.transform.forward * diffZ / 2f);
+        Other.AddForce(-_self.transform.forward * diffZ / 2f);
+    }
+}
